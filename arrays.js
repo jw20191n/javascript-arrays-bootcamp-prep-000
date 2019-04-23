c431f47752cc3d9773ef605b64650fcc9b54b9f1
Jiayi Wang <clairejw1015@gmail.com>
2019-04-23 22:13:05 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,10 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array.unshift(element);
 }
 
+function destructivelyAddElementToEndOfArray(array, element){
+  return array.push(element)
+}
+
 function addElementToBeginningOfArray(array, element){
  var newArray = [element,...array]
  return newArray;
